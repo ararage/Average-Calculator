50c66a6f212cb2583a54a4bcc58907cb47a4ffa1
krauserman <prz.perez.ric@gmail.com>
2018-04-16 22:26:04 -0500

    Initial commit

--- Average Calculator/ViewController.swift
+++ Average Calculator/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitleLAbelOUTLET: UILabel!
+    @IBOutlet weak var txtInputTextOUTLET: UITextField!
+    @IBOutlet weak var btnCalculateOUTLET: UIButton!
+    @IBOutlet weak var lblDisplayAverageOUTLET: UILabel!
+    @IBOutlet weak var btnResetOUTLET: UIButton!
+    
+    var average : Double = 0
+    var total : Double = 0
+    var count : Double = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressCalculateButton(_ sender: UIButton) {
+        if(!(txtInputTextOUTLET.text?.isEmpty)!){
+            var input = Double(txtInputTextOUTLET.text!)
+            count+=1
+            calculateAverage(inputNumber: input!)
+            displayAverage()
+            txtInputTextOUTLET.text?.removeAll()
+        }else{
+            lblDisplayAverageOUTLET.text = "You need to enter a number, silly!"
+        }
+    }
 
+    @IBAction func pressResetButton(_ sender: UIButton) {
+        resetValures()
+    }
+    
+    func calculateAverage(inputNumber : Double){
+        total+=inputNumber
+        average = total/count
+    }
+    
+    func displayAverage(){
+        lblDisplayAverageOUTLET.text = String(average)
+    }
+    
+    func resetValures(){
+        average = 0
+        total = 0
+        count = 0
+        lblDisplayAverageOUTLET.text = "Display average"
+    }
 }
 
